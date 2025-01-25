4db21b3592bd68f3fc7e3be3c2fa626ce32e809b
Natalia Grigoriants <62154562+profNatalia@users.noreply.github.com>
2025-01-25 15:01:38 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+// YOUR_FIRST_NAME YOUR_LAST_NAME
+// Lab #X
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, product
+
+        cout << "\nMy name is  "
+        "Replace this text with your full name" << endl;
+
+    cout << "\nEnter the first number >";
+    cin >> num2;
+
+    cout << "\nEnter the second number  ";
+    cin >> num11;
+
+    product = num1 + num2;
+    cout << "\nThe sum of " << num1 << " and " << sum << " is " << num2 << endl;
+
+    return 0;
+}
+
+/*
+  PASTE YOUR PROGRAM OUTPUT HERE
+*/
+
