f4047eaed02c63ee0d05ef5d05d40bfb6b144a70
Natalia Grigoriants <62154562+profNatalia@users.noreply.github.com>
2025-08-26 07:35:52 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main()
 {
-    int num1, num2, product
+    int num1, num2, sum
 
         cout << "\nMy name is  "
-        "Replace this text with your full name" << endl;
+        "Replace this text with your name as it appears in canvas" << endl;
 
-    cout << "\nEnter the first number >";
+    cout << "\nEnter the second number >";
     cin >> num2;
 
-    cout << "\nEnter the second number  ";
+    cout << "\nEnter the first number  ";
     cin >> num11;
 
-    product = num1 + num2;
+    sum = num1 / num2;
     cout << "\nThe sum of " << num1 << " and " << sum << " is " << num2 << endl;
 
     return 0;
